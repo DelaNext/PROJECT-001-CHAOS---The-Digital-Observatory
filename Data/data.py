378dd04fe2d46030f0ccca94f519eb60853250a6
Delano Castelo <delano.c.b.m.costa@gmail.com>
2026-09-14 19:53:10 -0300

    feat: add data module

---
+++ Data/data.py
@@ -0,0 +1,5 @@
+import Core.cpu
+
+CPU = Core.cpu.cpu # The second "cpu" refers to the dictionary defined inside cpu.py
+
+print(CPU) #Or print(CPU["usage"]) to print only the usage percentage or another key from the dictionary.
